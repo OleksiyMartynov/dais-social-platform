45b97a1a5bcc9b1448f8c6cbbc224bc5b1f699ac
John Z <john.zelskiy@gmail.com>
2019-09-26 22:50:08 -0400

    poa sokol deployment config

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,20 @@
 const path = require("path");
-
+var HDWalletProvider = require("truffle-hdwallet-provider");
+const MNEMONIC = "ribbon dial betray jungle flower leave piano grocery miss globe credit annual";
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "build/contracts"),
   networks: {
     development: {
       host: "127.0.0.1",
       port: 9545,
       network_id: "*" // Match any network id
+    },
+    sokol: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://poa.api.nodesmith.io/v1/sokol/jsonrpc?apiKey=1c17f373e6d3483c97410c3ba26f4cfd")
+      },
+      network_id: 77,
+      gas: 8000000  
     }
   },
   compilers: {
